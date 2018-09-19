f046b618f4358e7c8f0cfd0ec44b393753550177
Ibrahim Aksoy <ib.koray@gmail.com>
2018-09-19 14:59:16 +0300

    Imported menu-icon with font-awesome and updated style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
-import logo from './assets/Logo_1x.png';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./assets/Logo_1x.png";
+import "./App.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faBars);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="menu-icon">
+          <FontAwesomeIcon icon="bars" />
+        </div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p className="App-slogan">Rekabete karşı meydan okur!</p>
