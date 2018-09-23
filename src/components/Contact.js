b74d03aff53e6ce5e90c402b57ee1299e8bfcd6c
Ibrahim Aksoy <ib.koray@gmail.com>
2018-09-23 15:55:08 +0300

    Added contents to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,14 @@ class Contact extends Component {
     return (
       <div className="App-contact">
         <h1>İletişim</h1>
+        <div>
+          <h3>Çalışma Saatleri</h3>
+          <p>Pazartesi-Cuma: 9:00 – 17:00</p>
+          <h3>Telefon</h3>
+          <p> (0532) 374 10 70</p>
+          <h3>Email</h3>
+          <p>info@isikoren.com</p>
+        </div>
       </div>
     );
   }
