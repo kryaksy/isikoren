ed77541274f0a5082843ec6f9c4ab87482094373
Ibrahim Aksoy <ib.koray@gmail.com>
2018-10-22 11:46:33 +0300

    Imported Welcome component into Services page

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import Header from "../Header";
 import Footer from "../Footer";
+import Welcome from "../Welcome";
 import AllServices from "../AllServices";
 
 class Services extends Component {
@@ -9,6 +10,7 @@ class Services extends Component {
     return (
       <div id="services">
         <Header />
+        <Welcome />
         <AllServices />
         <Footer />
       </div>
