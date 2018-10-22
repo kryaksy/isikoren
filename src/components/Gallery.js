fca25331a9cb42d029da77ce671b0fd969404c98
Ibrahim Aksoy <ib.koray@gmail.com>
2018-10-22 08:16:30 +0300

    Updated elements of Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,29 +4,19 @@ class Gallery extends Component {
   render() {
     return (
       <div id="gallery">
-        <div className="line" />
-        <h2 className="title">Galeri</h2>
+        <div className="heading">
+            <div className="line" />
+            <h2 className="title">Galeri</h2>
+          </div>
         {/* <p className="statement">Enim anim sint sint laboris sunt.</p> */}
         <div className="collage">
           <div className="area-1">
             <div className="image-container" />
-            <div className="image-container" />
-            <div className="image-container" />
-          </div>
-          <div className="area-2">
-            <div className="image-container" />
-            <div className="image-container" />
+            <div className="image-container big" />
             <div className="image-container" />
           </div>
         </div>
-        <div className="buttons">
-          <div className="prev">
-            <div className="line" />
-          </div>
-          <div className="next">
-            <div className="line" />
-          </div>
-        </div>
+        <a href="#gallery" className="more">daha fazla</a>
       </div>
     );
   }
