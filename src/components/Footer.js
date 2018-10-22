457a05e47ce942cd9f4b0c02ad1f8d93c16ceb82
Ibrahim Aksoy <ib.koray@gmail.com>
2018-10-22 05:45:59 +0300

    Updated Footer content / added logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import logo from "../assets/logo.png";
 
 class Footer extends Component {
   render() {
     return (
       <div id="footer">
-        <div className="logo" />
+        <div className="logo">
+          <img src={logo} width="100" color="white" />
+        </div>
         <p className="slogan">Rekabete karşı meydan okur!</p>
         <div className="footlinks">
           <ul className="menu list">
@@ -17,43 +20,71 @@ class Footer extends Component {
           <ul className="follow list">
             <h2 className="title">Takip et</h2>
             <li className="menu-item">
-              <a href="https://www.facebook.com/isikoren" target="_blank">Facebook</a>
+              <a href="https://www.facebook.com/isikoren" target="_blank">
+                Facebook
+              </a>
             </li>
             <li className="menu-item">
-              <a href="https://twitter.com/hilmiisikoren" target="_blank">Twitter</a>
+              <a href="https://twitter.com/hilmiisikoren" target="_blank">
+                Twitter
+              </a>
             </li>
             <li className="menu-item">
-              <a href="https://www.instagram.com/hilmiisikoren" target="_blank">Instagram</a>
+              <a href="https://www.instagram.com/hilmiisikoren" target="_blank">
+                Instagram
+              </a>
             </li>
             <li className="menu-item">
-              <a href="https://www.linkedin.com/in/hilmiisikoren" target="_blank">Linkedin</a>
+              <a
+                href="https://www.linkedin.com/in/hilmiisikoren"
+                target="_blank"
+              >
+                Linkedin
+              </a>
             </li>
             <li className="menu-item">
-              <a href="https://www.youtube.com/c/hilmiisikorenblogger" target="_blank">Youtube</a>
+              <a
+                href="https://www.youtube.com/c/hilmiisikorenblogger"
+                target="_blank"
+              >
+                Youtube
+              </a>
             </li>
             <li className="menu-item">
-              <a href="https://tr.pinterest.com/hilmiisikoren" target="_blank">Pinterest</a>
+              <a href="https://tr.pinterest.com/hilmiisikoren" target="_blank">
+                Pinterest
+              </a>
             </li>
             <li className="menu-item">
-              <a href="https://www.flickr.com/photos/hilmiisikoren" target="_blank">Flickr</a>
+              <a
+                href="https://www.flickr.com/photos/hilmiisikoren"
+                target="_blank"
+              >
+                Flickr
+              </a>
             </li>
             <li className="menu-item">
-              <a href="https://vimeo.com/isikoren" target="_blank">Vimeo</a>
+              <a href="https://vimeo.com/isikoren" target="_blank">
+                Vimeo
+              </a>
             </li>
             <li className="menu-item">
-              <a href="https://plus.google.com/+HilmiI%C5%9F%C4%B1k%C3%B6renblogger" target="_blank">Google +</a>
+              <a
+                href="https://plus.google.com/+HilmiI%C5%9F%C4%B1k%C3%B6renblogger"
+                target="_blank"
+              >
+                Google +
+              </a>
             </li>
           </ul>
           <ul className="contact list">
             <h2 className="title">İletişim</h2>
             <li className="menu-item">info@isikoren.com</li>
-          </ul>
-          <ul className="next-page">
             <h2 className="title">Sonraki</h2>
-            <li className="menu-item">About</li>
+            <p className="menu-item">About</p>
           </ul>
         </div>
-        <div className="menu-icon" />
+        <div className="copyright">Copyrighted 2018</div>
       </div>
     );
   }
