ba2b3f99f5044c4b013980573e067963936cd405
Ibrahim Aksoy <ib.koray@gmail.com>
2018-10-24 06:25:01 +0300

    Updated menu-window page elements

--- src/components/MenuWindow.js
+++ src/components/MenuWindow.js
@@ -1,86 +1,56 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class MenuWindow extends Component {
   render() {
     return (
       <div id="menuWindow">
-        <div className="logo" />
-        <div className="close button" />
-        <ul className="menu-list">
-          <li className="menu-item">
-            <span className="line" />
-            Ana Sayfa
-          </li>
-          <li className="menu-item">
-            <span className="line" />
-            Biz Kimiz?
-          </li>
-          <li className="menu-item">
-            <span className="line" />
-            Servisler
-          </li>
-          <li className="menu-item">
-            <span className="line" />
-            İletişim
-          </li>
-        </ul>
+        <Header />
         <p className="slogan">Rekabete karşı meydan okur.</p>
-        <ul className="follow icons">
-          <li className="item">
-            <a href="https://www.facebook.com/isikoren" target="_blank">
-              Facebook
-            </a>
+        <ul className="follow-list flex">
+          <li className="icon">
+            <a href="https://www.facebook.com/isikoren" target="_blank" />
           </li>
-          <li className="item">
-            <a href="https://twitter.com/hilmiisikoren" target="_blank">
-              Twitter
-            </a>
+          <li className="icon">
+            <a href="https://twitter.com/hilmiisikoren" target="_blank" />
           </li>
-          <li className="item">
-            <a href="https://www.instagram.com/hilmiisikoren" target="_blank">
-              Instagram
-            </a>
+          <li className="icon">
+            <a href="https://www.instagram.com/hilmiisikoren" target="_blank" />
           </li>
-          <li className="item">
-            <a href="https://www.linkedin.com/in/hilmiisikoren" target="_blank">
-              Linkedin
-            </a>
+          <li className="icon">
+            <a
+              href="https://www.linkedin.com/in/hilmiisikoren"
+              target="_blank"
+            />
           </li>
-          <li className="item">
+          <li className="icon">
             <a
               href="https://www.youtube.com/c/hilmiisikorenblogger"
               target="_blank"
-            >
-              Youtube
-            </a>
+            />
           </li>
-          <li className="item">
-            <a href="https://tr.pinterest.com/hilmiisikoren" target="_blank">
-              Pinterest
-            </a>
+          <li className="icon">
+            <a href="https://tr.pinterest.com/hilmiisikoren" target="_blank" />
           </li>
-          <li className="item">
+          <li className="icon">
             <a
               href="https://www.flickr.com/photos/hilmiisikoren"
               target="_blank"
-            >
-              Flickr
-            </a>
+            />
           </li>
-          <li className="item">
-            <a href="https://vimeo.com/isikoren" target="_blank">
-              Vimeo
-            </a>
+          <li className="icon">
+            <a href="https://vimeo.com/isikoren" target="_blank" />
           </li>
-          <li className="item">
+          <li className="icon">
             <a
               href="https://plus.google.com/+HilmiI%C5%9F%C4%B1k%C3%B6renblogger"
               target="_blank"
-            >
-              Google +
-            </a>
+            />
           </li>
         </ul>
+        <p className="statement">
+          Cupidatat dolore quis reprehenderit quis deserunt.
+        </p>
       </div>
     );
   }
