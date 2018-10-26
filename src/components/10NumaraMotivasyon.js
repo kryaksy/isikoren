b657bc753cb51615be6628750d950180a8607259
Ibrahim Aksoy <ib.koray@gmail.com>
2018-10-26 05:21:49 +0300

    Updated 10NumaraMotivasyon content

--- src/components/10NumaraMotivasyon.js
+++ src/components/10NumaraMotivasyon.js
@@ -4,14 +4,16 @@ class OnNumaraMotivasyon extends Component {
   render() {
     return (
       <div id="onNumaraMotivasyon">
-        <h2 className="title">10 Numara Motivasyon</h2>
-        <p className="snippet">
-          Hamurunda sevgi, temelinde motivasyon olan iş ve hayata dair
-          paylaşımlarımızın insanlara ilham vermesi amacıyla yayındayız.
-          Sayfamızdaki tüm içerikler kendi özgün fikir ve tavsiyelerimizden
-          oluşmaktadır.
-        </p>
-        <button>Sayfaya Git</button>
+        <div className="container">
+          <h2 className="title">10 Numara Motivasyon</h2>
+          <p className="snippet">
+            Hamurunda sevgi, temelinde motivasyon olan iş ve hayata dair
+            paylaşımlarımızın insanlara ilham vermesi amacıyla yayındayız.
+            Sayfamızdaki tüm içerikler kendi özgün fikir ve tavsiyelerimizden
+            oluşmaktadır.
+          </p>
+          <button>Sayfaya Git</button>
+        </div>
       </div>
     );
   }
