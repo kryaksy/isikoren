f11fce56fed9e1d742f1b18a1fa1ff616521da11
Ibrahim Aksoy <ib.koray@gmail.com>
2018-10-26 06:55:09 +0300

    Reordered Team elements

--- src/components/Team.js
+++ src/components/Team.js
@@ -14,23 +14,27 @@ class Team extends Component {
         <ul className="list">
           <li className="item">
             <img className="picture" />
-            <h3 className="name">Hilmi Işıkören</h3>
-            <p className="pitch">
-              1997 – 2010 tarihleri arasında uluslararası üç gayrimenkul
-              markasındaki üst düzey yöneticilik deneyimiyle de gayrimenkul ve
-              inşaat sektörün önde gelen ismidir.
-            </p>
-            {/* Learn More button might be needed */}
+            <div className="content">
+              <h3 className="name">Hilmi Işıkören</h3>
+              <p className="snippet">
+                1997 – 2010 tarihleri arasında uluslararası üç gayrimenkul
+                markasındaki üst düzey yöneticilik deneyimiyle de gayrimenkul ve
+                inşaat sektörün önde gelen ismidir.
+              </p>
+              {/* Learn More button might be needed */}
+            </div>
           </li>
           <li className="item">
             <img className="picture" />
-            <h3 className="name">Belgin Benek</h3>
-            <p className="pitch">
-              Işıkören Danışmanlık şirketinin kurucu ortağı Belgin Benek,
-              iletişim ve pazarlama konusunda uzmanlığının yanı sıra yetişkin
-              eğitimcisi olarak da mesleki yetkinliğe sahiptir.
-            </p>
-            {/* Learn More button might be needed */}
+            <div className="content">
+              <h3 className="name">Belgin Benek</h3>
+              <p className="snippet">
+                Işıkören Danışmanlık şirketinin kurucu ortağı Belgin Benek,
+                iletişim ve pazarlama konusunda uzmanlığının yanı sıra yetişkin
+                eğitimcisi olarak da mesleki yetkinliğe sahiptir.
+              </p>
+              {/* Learn More button might be needed */}
+            </div>
           </li>
         </ul>
       </div>
