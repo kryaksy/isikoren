11ef74f7cf730232f22a7937254ece10d25c8dc3
Ibrahim Aksoy <ib.koray@gmail.com>
2018-10-26 04:52:06 +0300

    Added new content div for each proposition item in WhatWeDo
    Content is wrapped into it

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -12,66 +12,78 @@ class WhatWeDo extends Component {
           <li className="list-item">
             {/* Image or video */}
             <div className="media" />
-            <h3 className="title">Startup projeniz mi var?</h3>
-            <p className="snippet">
-              Yeni girişimlerin çoğu başarısızlıkla sonuçlanıyor. Farklılaşıp
-              öne çıkmak, başarıyla tanışmak istiyorsanız yanınızdayız.
-            </p>
+            <div className="content">
+              <h3 className="title">Startup projeniz mi var?</h3>
+              <p className="snippet">
+                Yeni girişimlerin çoğu başarısızlıkla sonuçlanıyor. Farklılaşıp
+                öne çıkmak, başarıyla tanışmak istiyorsanız yanınızdayız.
+              </p>
+            </div>
           </li>
           <li className="list-item">
             {/* Image or video */}
             <div className="media" />
-            <h3 className="title">
-              İnşaat projenizdeki satış ofisiniz için eğitim mi almak
-              istiyorsunuz?
-            </h3>
-            <p className="snippet">
-              İnşaat firmalarının proje satış ofislerinde çalışan satış
-              ekiplerine özel eğitim ve koçluk programlarımızla yanınızdayız.
-            </p>
+            <div className="content">
+              <h3 className="title">
+                İnşaat projenizdeki satış ofisiniz için eğitim mi almak
+                istiyorsunuz?
+              </h3>
+              <p className="snippet">
+                İnşaat firmalarının proje satış ofislerinde çalışan satış
+                ekiplerine özel eğitim ve koçluk programlarımızla yanınızdayız.
+              </p>
+            </div>
           </li>
           <li className="list-item">
             {/* Image or video */}
             <div className="media" />
-            <h3 className="title">Markalaşmak mı istiyorsunuz?</h3>
-            <p className="snippet">
-              İsminizi marka yapmak, sadık müşterilerden oluşan güvenilir bir
-              marka olmak istiyorsanız yanınızdayız.
-            </p>
+            <div className="content">
+              <h3 className="title">Markalaşmak mı istiyorsunuz?</h3>
+              <p className="snippet">
+                İsminizi marka yapmak, sadık müşterilerden oluşan güvenilir bir
+                marka olmak istiyorsanız yanınızdayız.
+              </p>
+            </div>
           </li>
           <li className="list-item">
             {/* Image or video */}
             <div className="media" />
-            <h3 className="title">
-              İnşaat firmanız için danışmanlık mı almak istiyorsunuz?
-            </h3>
-            <p className="snippet">
-              İnşaat firmanızın gayrimenkul projelerini satışa açmadan önce
-              profesyonel stratejik iletişim, markalaşma, pazarlama ve satış
-              danışmanlığı hizmeti almak isterseniz yanınızdayız.
-            </p>
+            <div className="content">
+              <h3 className="title">
+                İnşaat firmanız için danışmanlık mı almak istiyorsunuz?
+              </h3>
+              <p className="snippet">
+                İnşaat firmanızın gayrimenkul projelerini satışa açmadan önce
+                profesyonel stratejik iletişim, markalaşma, pazarlama ve satış
+                danışmanlığı hizmeti almak isterseniz yanınızdayız.
+              </p>
+            </div>
           </li>
           <li className="list-item">
             {/* Image or video */}
             <div className="media" />
-            <h3 className="title">
-              Emlak ofisiniz için eğitim mi almak istiyorsunuz?
-            </h3>
-            <p className="snippet">
-              Emlak ofislerinize özel motivasyon temelli eğitim programlarımız
-              ile yanınızdayız.
-            </p>
+            <div className="content">
+              <h3 className="title">
+                Emlak ofisiniz için eğitim mi almak istiyorsunuz?
+              </h3>
+              <p className="snippet">
+                Emlak ofislerinize özel motivasyon temelli eğitim programlarımız
+                ile yanınızdayız.
+              </p>
+            </div>
           </li>
           <li className="list-item">
             {/* Image or video */}
             <div className="media" />
-            <h3 className="title">
-              Motivasyon üzerine seminer mi almak istiyorsunuz?
-            </h3>
-            <p className="snippet">
-              Şirket için motivasyon, başarı için yüreklendirici, ekibinize
-              ilham veren konuşmalar, seminerler için yanınızdayız.
-            </p>
+            <div className="content">
+              <h3 className="title">
+                Motivasyon üzerine seminer mi almak istiyorsunuz?
+              </h3>
+              <p className="snippet">
+                Şirket için motivasyon, başarı için yüreklendirici, ekibinize
+                ilham veren konuşmalar, seminerler için yanınızdayız.
+              </p>
+            </div>
           </li>
         </ul>
       </div>
