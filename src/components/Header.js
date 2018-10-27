ee510f57f872944522c105581c59010f44601367
Ibrahim Aksoy <ib.koray@gmail.com>
2018-10-28 00:21:13 +0300

    Wrapped menu items into Links
    Added Link feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import "./Header.css";
 
@@ -12,19 +13,27 @@ class Header extends Component {
         <div className="navigation flex">
           <ul className="menu-list">
             <li className="menu-item active">
-              <p>Ana Sayfa</p>
+              <Link to="/">
+                <p>Ana Sayfa</p>
+              </Link>
               <div className="line" />
             </li>
             <li className="menu-item">
-              <p>Biz Kimiz</p>
+              <Link to="/about">
+                <p>Biz Kimiz</p>
+              </Link>
               <div className="line" />
             </li>
             <li className="menu-item">
-              <p>Servisler</p>
+              <Link to="/services">
+                <p>Servisler</p>
+              </Link>
               <div className="line" />
             </li>
             <li className="menu-item">
-              <p>İletişim</p>
+              <Link to="/contact">
+                <p>İletişim</p>
+              </Link>
               <div className="line" />
             </li>
           </ul>
