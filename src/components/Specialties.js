a66540024b70b76d9906f0a1ec90f6ef79325486
Ibrahim Aksoy <ib.koray@gmail.com>
2018-10-27 23:55:26 +0300

    Fixed anchor link

--- src/components/Specialties.js
+++ src/components/Specialties.js
@@ -11,7 +11,7 @@ class Specialties extends Component {
             <h2 className="title">Uzmanlık Alanlarımız</h2>
           </div>
           <div className="linking">
-            <a>Servislere gözat</a>
+            <a href="/services">Servislere gözat</a>
             <div className="line" />
           </div>
         </div>
