619e74371d6b703ea1d13269f35f3eea9cdd759e
Ibrahim Aksoy <ib.koray@gmail.com>
2018-10-29 09:28:26 +0200

    Updates

---
+++ precache-manifest.2b2784a6f1142f93dca48162380a7162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b494436a10066d0dec5250938cec4a9",
+    "url": "/isikoren/static/media/logo.0b494436.png"
+  },
+  {
+    "revision": "599fdc5d808bacb286eb",
+    "url": "/isikoren/static/js/runtime~main.599fdc5d.js"
+  },
+  {
+    "revision": "5440a99a96d7b65ae153",
+    "url": "/isikoren/static/js/main.5440a99a.chunk.js"
+  },
+  {
+    "revision": "163537e1cfaec4b9936e",
+    "url": "/isikoren/static/js/1.163537e1.chunk.js"
+  },
+  {
+    "revision": "5440a99a96d7b65ae153",
+    "url": "/isikoren/static/css/main.da3d4767.chunk.css"
+  },
+  {
+    "revision": "96393114e7c2ce5a255f56c2f0b6d15b",
+    "url": "/isikoren/index.html"
+  }
+];
