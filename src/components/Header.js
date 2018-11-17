42edf1b589e8babbf66efcd68700605a187ee0fd
Ibrahim Aksoy <ib.koray@gmail.com>
2018-11-17 23:21:37 +0200

    Added container and necessary classes with conditional rendering

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,32 +5,50 @@ import "./Header.css";
 import * as wpAPI from "./../wpAPI";
 
 class Header extends Component {
+  state = {
+    minimized: false,
+    visible: true
+  };
+
   render() {
     const { company, menu } = wpAPI.sampleAPI;
 
     return (
-      <div id="header">
-        <div className="logo">
-          <Link to="/">
-            <img src={logo} height="40" alt={company.logo.alt} />
-          </Link>
-        </div>
-        <div className="navigation flex">
-          <ul className="menu-list">
-            {menu.map(item => (
-              <li key={item.slug} className="menu-item">
-                <Link to={item.slug}>
-                  <p>{item.title}</p>
-                </Link>
-                <div className="line" />
-              </li>
-            ))}
-          </ul>
-          <div className="icon-container">
-            <div className="menu-icon">
-              <span />
-              <span />
-              <span />
+      <div
+        id="header"
+        className={
+          (this.state.minimized ? "mini" : "") +
+          " " +
+          (this.state.visible ? "" : "hide")
+        }
+      >
+        <div className="container">
+          <div className="logo">
+            <Link to="/">
+              <img
+                src={logo}
+                height={this.state.minimized ? "30" : "40"}
+                alt={company.logo.alt}
+              />
+            </Link>
+          </div>
+          <div className="navigation flex">
+            <ul className="menu-list">
+              {menu.map(item => (
+                <li key={item.slug} className="menu-item">
+                  <Link to={item.slug}>
+                    <p>{item.title}</p>
+                  </Link>
+                  <div className="line" />
+                </li>
+              ))}
+            </ul>
+            <div className="icon-container">
+              <div className="menu-icon">
+                <span />
+                <span />
+                <span />
+              </div>
             </div>
           </div>
         </div>
