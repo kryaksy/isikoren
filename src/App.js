9187d5935b0547c6db88aba547337b461cf51840
Ibrahim Aksoy <ib.koray@gmail.com>
2018-12-11 02:49:58 +0300

    Refactored AppJS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import React, {Component} from "react";
+import {Route} from "react-router-dom";
 import {AnimatedSwitch} from "react-router-transition";
 import * as wpAPI from "./wpAPI";
-
 //PAGES
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Services from "./components/pages/Services";
 import Contact from "./components/pages/Contact";
-
 // SECTIONS
 import Biography from "./components/Biography";
 import Service from "./components/Service";
-import MenuWindow from "./components/MenuWindow";
 import Footer from "./components/Footer";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
+	render() {
+		return (
+      <div className="App">
   			<AnimatedSwitch atEnter={{
   					opacity: 0
   				}} atLeave={{
@@ -26,30 +23,24 @@ class App extends Component {
   				}} atActive={{
   					opacity: 1
   				}} className="switch-wrapper">
+    				<Route exact="exact" path="/" component={Home}/>
+    				<Route path="/about" component={About}/>
+    				<Route path="/services" component={Services}/>
+    				<Route path="/contact" component={Contact}/>
 
-        {/* PAGES */}
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/services" component={Services} />
-        <Route path="/contact" component={Contact} />
+            {"Biography"}
+            {wpAPI.sampleAPI.team.map(member => (
+              <Route key={member.slug} path={member.slug} component={Biography}/>
+            ))}
 
-        {/* PROFILES */}
-        {wpAPI.sampleAPI.team.map( member => (
-          <Route key={member.slug} path={member.slug} component={Biography} />
-        ))}
-
-        {/* SERVICES */}
-        {wpAPI.sampleAPI.services.map( service => (
-          <Route key={service.slug} path={service.slug} component={Service} />
-        ))}
+            {"Service"}
+    				{wpAPI.sampleAPI.services.map(service => (
+              <Route key={service.slug} path={service.slug} component={Service}/>
+            ))}
   			</AnimatedSwitch>
-        {/* MENU WINDOW */}
-        <MenuWindow />
-        <Footer />
 
-      </div>
-        );
-    }
+  			<Footer/>
+  		</div>);
+	}
 }
-
 export default App;
